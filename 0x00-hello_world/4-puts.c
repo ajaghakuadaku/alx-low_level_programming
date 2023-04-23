ff0bf032918eeceb0c7de96cac0617178231f33c
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-04-23 08:57:31 -0700

    correct

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ * main -  print using puts followed by a new line.
  * Return: 0 (success)
  */
 int main(void)
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print using printf, followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
