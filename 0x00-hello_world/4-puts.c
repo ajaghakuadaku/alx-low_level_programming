e34fb315f14ceab2a34093b438a15d56cf9ea70e
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-04-23 08:33:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main -  a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 
