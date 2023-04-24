c2ced8d4255cf3e5590804dc8d9ef151fdac019a
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-04-24 10:07:29 -0700

    ll

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - program that prints the alphabet in lowercase
+* Return: Always (Success)
+*/
+int main(void)
+{
+	char low_upper;
+	
+	for (low_upper = 'a'; low_upper <= 'z'; low_upper++)
+	{
+	putchar(low_upper);
+
+	for (low_upper = 'A'; low_upper <= 'Z'; low_upper++)
+	{
+	putchar(low_upper);
+	putchar('\n');
+	return (0);
+}
+
