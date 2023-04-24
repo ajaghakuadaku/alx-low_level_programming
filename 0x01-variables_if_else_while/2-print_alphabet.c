cf9c437cc812af15b957f5d23d61fbc0a05199ff
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-04-24 07:27:17 -0700

    variab

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - program that prints the alphabet in lowercase
+* Return: Always (Success)
+*/
+int main(void)
+{
+	char alphb;
+
+	for (alphb = 'a'; alphb <= 'z'; alphb++)
+	putchar(alphb);
+	putchar('\n');
+	return (0);
+}
+
