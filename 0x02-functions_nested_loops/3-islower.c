9a35e03b63781092619bfad063dc2ef203ea7f0d
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-04-26 06:49:15 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: character to be checked
+ * Return: 1 for lowercase 0 not lowercase
+ */
+
+int _islower(int c)
+{
+
+	if (c >= '97' && c <= '122)
+	{
+
+	return (1);
+	}
+	return(0);
+}
