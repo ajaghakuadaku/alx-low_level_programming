205a5e2118f0c83144262b41831a065de13bb886
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-05-01 04:01:22 -0700

    gut

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - function that checks for uppercase character
+ * @c: value to be checked
+ * Return: 1 if uppercase and 0 for any other
+ */
+
+int _isupper(int c)
+{
+
