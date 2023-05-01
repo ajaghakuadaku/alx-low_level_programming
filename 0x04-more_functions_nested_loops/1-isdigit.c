77cf3529716220a1d43d7f1257d34372ac68713a
ajaghakuadaku <ajaghaku_adaku@yahoo.com>
2023-05-01 05:25:26 -0700

    jou

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: character to be checked
+ * Return: success 1 but 0 if otherwise'
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
